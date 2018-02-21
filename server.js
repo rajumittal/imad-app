e6ad131eb3047325a019b48ced1da1a5a2882745
Raju kumar <36443109+rajumittal@users.noreply.github.com>
2018-02-22 00:02:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('artical-one',function(req,res){
+    res.send('artical one requested and will be served here');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
